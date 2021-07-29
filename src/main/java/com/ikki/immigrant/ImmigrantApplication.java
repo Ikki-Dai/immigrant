6eb294fada789dcc93a038e1dca335663d0c9fc3
ikki dai <ikkiDay@163.com>
2021-07-29 23:23:16 +0800

    <arch>(all):init a java project
    1. add necessary dependencies
    2. add gitignore
    3. add main bootstrap entry

---
+++ src/main/java/com/ikki/immigrant/ImmigrantApplication.java
@@ -0,0 +1,13 @@
+package com.ikki.immigrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmigrantApplication.class, args);
+    }
+
+}
