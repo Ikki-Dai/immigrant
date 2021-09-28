aea26bee53f4930a508e1eb76a67c00724191803
ikki dai <ikkiDay@163.com>
2021-09-28 20:56:06 +0800

    <feat>(mapstruct): use mapstruct with lombok
    1. add mapstruct dependencies
    2. coperate with lombok
    3. use -parameter compile arg

---
+++ src/test/java/com/ikki/immigrant/infrastructure/util/LombokAndMapStructTest.java
@@ -0,0 +1,77 @@
+package com.ikki.immigrant.infrastructure.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+public class LombokAndMapStructTest {
+
+
+    @Test
+    void test() {
+        Car car = new Car();
+        car.setMake("China");
+        car.setType(CarType.SEDAN);
+        car.setNumberOfSeats(3);
+        CarDto carDto = CarMapper.INSTANCE.carToCarDto(car);
+        System.out.println(carDto);
+        Assertions.assertNotNull(carDto);
+    }
+
+    @Test
+    void testEnum() {
+        CarDto carDto = new CarDto();
+        carDto.setMake("China");
+        carDto.setType("SEDAN");
+        carDto.setSeatCount(4);
+        Car car = CarMapper.INSTANCE.carDtoToCar(carDto);
+        Assertions.assertNotNull(car);
+    }
+
+
+    public enum CarType {
+        SEDAN
+    }
+
+    @Mapper()
+    public interface CarMapper {
+
+        CarMapper INSTANCE = Mappers.getMapper(CarMapper.class);
+
+        @Mapping(source = "numberOfSeats", target = "seatCount")
+        CarDto carToCarDto(Car car);
+
+        @Mapping(source = "seatCount", target = "numberOfSeats")
+        Car carDtoToCar(CarDto carDto);
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Car {
+
+        private String make;
+        private int numberOfSeats;
+        private CarType type;
+
+        //constructor, getters, setters etc.
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class CarDto {
+
+        private String make;
+        private int seatCount;
+        private String type;
+
+        //constructor, getters, setters etc.
+    }
+
+}
