6923f060f097f208c29793927dc06c47d494493a
ikkiday <ikkiday@163.com>
2021-11-30 15:04:29 +0800

    <fix>(ut): fix i18n unit test
    1. use @SpringBootTest
    2. use Simpled_Chinese

--- src/test/java/com/ikki/immigrant/infrastructure/i18n/I18nTest.java
+++ src/test/java/com/ikki/immigrant/infrastructure/i18n/I18nTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 
 import java.util.Locale;
@@ -12,7 +12,7 @@ import java.util.Locale;
 /**
  * @author ikki
  */
-@WebMvcTest
+@SpringBootTest
 class I18nTest {
 
     @Autowired
@@ -20,19 +20,19 @@ class I18nTest {
 
     @BeforeEach
     void prepare() {
-        Assertions.assertNotNull(messageSource);
+        Assertions.assertNotNull(messageSource);    
     }
 
     @Test
     void messageSourceTest() {
-        String s = messageSource.getMessage("test_key", null, Locale.CHINESE);
+        String s = messageSource.getMessage("test_key", null, Locale.SIMPLIFIED_CHINESE);
         System.out.println(s);
         Assertions.assertEquals("测试消息", s);
     }
 
     @Test
     void messageSourceWithArgsTest() {
-        String s = messageSource.getMessage("test_args", new Object[]{"客户"}, Locale.CHINESE);
+        String s = messageSource.getMessage("test_args", new Object[]{"客户"}, Locale.SIMPLIFIED_CHINESE);
         System.out.println(s);
         Assertions.assertEquals("亲爱的 客户", s);
     }
